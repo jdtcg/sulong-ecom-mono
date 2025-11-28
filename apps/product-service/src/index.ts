22a322aa51fe97ca39d4181edae0e0375d67c261
jdt <jdt@DESKTOP-GILBQ8U>
2025-11-28 08:31:17 +0800

    initial

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+)
+
+app.listen(8000, () => {
+  console.log("Product service is running on port 8000")
+})
